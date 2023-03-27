a6d57d6806cc7ad9f8aed7a804a9482427861459
elemouellic <emmanuel.lm@gmail.com>
2023-03-27 15:00:35 +0200

    Move index

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
 
 // Dotenv
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
