6e29b98df0e1ecd0ec89be63206b609db5b14488
elemouellic <emmanuel.lm@gmail.com>
2023-03-27 14:47:46 +0200

    Create 404

--- public/index.php
+++ public/index.php
@@ -2,5 +2,13 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Dotenv
+require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 
 echo 'Hello World';
