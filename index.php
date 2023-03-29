bc00a8e56bf88ea7e3f3e18652fe7a11c8b4ad1e
elemouellic <emmanuel.lm@gmail.com>
2023-03-29 08:41:21 +0200

    Modif Organisation

--- index.php
+++ index.php
@@ -1,11 +1,9 @@
 <?php
-
+//Chargment Autoload
 require __DIR__ . '/vendor/autoload.php';
 
-// Dotenv
-require_once __DIR__ . '/vendor/autoload.php';
+//Dotenv
 use Dotenv\Dotenv;
-
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
---
+++ src/models/PDO.php
@@ -0,0 +1,36 @@
+<?php
+namespace Locabraz\models;
+
+use Dotenv\Dotenv;
+use PDO;
+use PDOException;
+
+class Database {
+    private $pdo;
+
+    public function __construct() {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+        $dotenv->load();
+
+        $host = $_ENV['DB_HOST'];
+        $dbname = $_ENV['DB_NAME'];
+        $username = $_ENV['DB_USERNAME'];
+        $password = $_ENV['DB_PASSWORD'];
+        $port = $_ENV['DB_PORT'];
+
+
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $port);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Erreur de connexion : " . $e->getMessage());
+        }
+    }
+
+    public function getPdo() {
+        return $this->pdo;
+    }
+
+}
+
+?>
