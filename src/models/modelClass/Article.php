b32082ac811a2efc1b148ce8278b1a095fed7063
elemouellic <emmanuel.lm@gmail.com>
2023-04-06 13:51:55 +0200

    20230406 13:51

--- src/models/modelClass/Article.php
+++ src/models/modelClass/Article.php
@@ -1,72 +1,117 @@
 <?php
 
-// class Article {
-//    private $id;
-//    private $title;
-//    private $content;
-//    private $publishDate;
-//    private $photoId;
+namespace Locabraz\models\modelClass;
 
-//    public function __construct($id, $title, $content, $publishDate, $photoId) {
-//       $this->id = $id;
-//       $this->title = $title;
-//       $this->content = $content;
-//       $this->publishDate = $publishDate;
-//       $this->photoId = $photoId;
-//    }
+use Locabraz\models\DbConnector;
 
-//    public function getId() {
-//       return $this->id;
-//    }
 
-//    public function getTitle() {
-//       return $this->title;
-//    }
+/**
+ * *****Liste des fonctions*****
+ * insertArticle (créer un nouvel article et l'insérer dans la base de données)
+ * udpateArticle (mettre à jour un article dans la base de données)
+ * deleteArticle (supprimer un article de la base de données)
+ * getAllArticles (récupérer tous les articles de la base de données)
+ * getArticleById (récupérer un article en fonction de son ID)
+ */
 
-//    public function getContent() {
-//       return $this->content;
-//    }
+class Article extends DbConnector
+{
 
-//    public function getPublishDate() {
-//       return $this->publishDate;
-//    }
+    /** Créer un nouvel article **/
 
-//    public function getPhotoId() {
-//       return $this->photoId;
-//    }
+    public function insertArticle($title, $content, $publishdate, $idPhotoarticle = null)
+    {
+        $db = self::dbConnect();
 
-//    public function setTitle($title) {
-//       $this->title = $title;
-//    }
+        $req = $db->prepare(
+            "INSERT INTO article (
+            title, 
+            content, 
+            publishdate, 
+            idPhotoarticle
+            ) 
+            VALUES (:title, :content, :publishdate, :idPhotoarticle)"
+        );
 
-//    public function setContent($content) {
-//       $this->content = $content;
-//    }
+        $req->execute([
+            ':title' => $title,
+            ':content' => $content,
+            ':publishdate' => $publishdate,
+            ':idPhotoarticle' => $idPhotoarticle
+        ]);
+    }
 
-//    public function setPublishDate($publishDate) {
-//       $this->publishDate = $publishDate;
-//    }
+    /** Mettre à jour un article **/
 
-//    public function setPhotoId($photoId) {
-//       $this->photoId = $photoId;
-//    }
+    public function updateArticle($id, $title, $content, $publishdate, $idPhotoarticle = null)
+    {
+        $db = self::dbConnect();
 
-//    public function getFormattedPublishDate($format) {
-//       return date($format, strtotime($this->publishDate));
-//    }
+        $req = $db->prepare(
+            "UPDATE article 
+            SET title = :title, 
+            content = :content, 
+            publishdate = :publishdate, 
+            idPhotoarticle = :idPhotoarticle 
+            WHERE idArticles = :id"
+        );
+        $req->execute([
+            ':id' => $id,
+            ':title' => $title,
+            ':content' => $content,
+            ':publishdate' => $publishdate,
+            ':idPhotoarticle' => $idPhotoarticle
+        ]);
+    }
 
-//    public function getExcerpt($length) {
-//       $excerpt = strip_tags($this->content);
-//       if (strlen($excerpt) > $length) {
-//          $excerpt = substr($excerpt, 0, $length) . '...';
-//       }
-//       return $excerpt;
-//    }
+    /** Supprimer un article **/
 
-//    public function hasPhoto() {
-//       return !empty($this->photoId);
-//    }
+    public function deleteArticle($id)
+    {
+        $db = self::dbConnect();
 
-// }
+        $req = $db->prepare(
+            "DELETE FROM article 
+            WHERE idArticles = :id"
+        );
+        $req->execute([
+            ':id' => $id
+        ]);
+    }
 
-// ?>
+    /** Récupérer tous les articles **/
+
+    public function getAllArticles()
+    {
+        $db = self::dbConnect();
+
+        $req = $db->prepare("SELECT * FROM article");
+        $req->execute();
+
+        $articles = $req->fetchAll();
+
+        if (!$articles) {
+            throw new \Exception('Aucun article trouvé');
+        }
+
+        return $articles;
+    }
+
+    /** Récupérer un article en fonction de son ID **/
+
+    public function getArticleById($id)
+    {
+        $db = self::dbConnect();
+
+        $req = $db->prepare("SELECT * FROM article WHERE idArticles = :id");
+        $req->execute([':id' => $id]);
+
+        $article = $req->fetch();
+
+        if (!$article) {
+            throw new \Exception('Article non trouvé');
+        }
+
+        return $article;
+    }
+}
