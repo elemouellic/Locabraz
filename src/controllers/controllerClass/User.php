29942ec3d85b6b8ca16cdaaaa0884ed59594e5ba
elemouellic <emmanuel.lm@gmail.com>
2023-04-11 11:47:45 +0200

    20230411 11:47

--- src/controllers/controllerClass/User.php
+++ src/controllers/controllerClass/User.php
@@ -7,6 +7,7 @@ use Locabraz\models\modelClass\User;
 
 /**
  * *****Liste des méthodes*****
+ * loginUser (Connexion de l'utilisateur)
  * createUser (créer nouvel utilisateur et l'ajouter dans la base de données)
  * 
  */
@@ -58,6 +59,36 @@ class UserController extends MainController
         $user->insertUser($email, $name, $firstname, $phone, $address, $zipcode, $password);
 
         //Redirection vers le compte utilisateur
+        header('Location: views/frontpages/user/account.php');
+    }
+
+    /** Mettre à jour les informations de l'utilisateur */
+
+    public function upgradeUser()
+    {
+        // Vérification si l'utilisateur est connecté
+        session_start();
+        if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+            header('Location: views/frontpages/user/login.php');
+            exit;
+        }
+
+        $email = $_SESSION['email'];
+        $name = $_POST['name'];
+        $firstname = $_POST['firstname'];
+        $phone = $_POST['phone'];
+        $address = $_POST['address'];
+        $zipcode = $_POST['zipcode'];
+
+        try {
+            $user = new User();
+            $user->updateUser($email, $name, $firstname, $phone, $address, $zipcode);
+        } catch (\Exception $e) {
+            echo 'Erreur : ' . $e->getMessage();
+            return;
+        }
+
+        // Redirection vers le compte utilisateur
         header('Location: views/frontpages/user/account.php');
     }
 }
